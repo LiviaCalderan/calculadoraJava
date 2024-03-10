a10576442d693766d1edd8e11e68857152bacce4
Lívia Calderan <liv_calderan@hotmail.com>
2024-03-10 00:11:06 -0400

    Lógica da calculadora #05

--- calculadora/src/br/com/calderan/calc/modelo/Memoria.java
+++ calculadora/src/br/com/calderan/calc/modelo/Memoria.java
@@ -40,21 +40,50 @@ public class Memoria {
 
 		System.out.println(tipoComando);
 
-		if (TipoComando == null) {
+		if (tipoComando == null) {
 			return;
 		} else if (tipoComando == TipoComando.ZERAR) {
 			textoAtual = "";
 			textoBuffer = "";
 			substituir = false;
 			ultimaOperacao = null;
-		} else if (tipoComando = TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+		} else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
 			textoAtual = substituir ? texto : textoAtual + texto;
 			substituir = false;
 		} else {
-
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
 		}
 
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private String obterResultadoOperacao() {
+		if (ultimaOperacao == null) {
+			return textoAtual;
+		}
+
+		double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+		double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+
+		double resultado = 0;
+
+		if (ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		} else if (ultimaOperacao == TipoComando.SUBTRACAO) {
+			resultado = numeroBuffer - numeroAtual;
+		} else if (ultimaOperacao == TipoComando.MULTIPLICACAO) {
+			resultado = numeroBuffer * numeroAtual;
+		} else if (ultimaOperacao == TipoComando.DIVISAO) {
+			resultado = numeroBuffer / numeroAtual;
+		}
+
+		String resultString = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = resultString.endsWith(",0");
+
+		return inteiro ? resultString.replace(",0", "") : resultString;
 	}
 
 	private TipoComando detectarTipoComando(String texto) {
